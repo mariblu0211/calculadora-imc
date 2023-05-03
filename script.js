42fd5bc85d037005e4e39074a4f597e03ea26f09
Maria Luiza <129173352+mariblu0211@users.noreply.github.com>
2023-05-03 10:13:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,39 +10,39 @@ btn_calcular.addEventListener("click", function (e) {
 
     if (sexo.value == 'masculino') {
         if (imc < 18.5) {
-            imagemresultado.innerHTML = "<img src='images/m-magreza.png' alt='Magreza' >"
+            imagemresultado.innerHTML = ´<img src='images/m-magreza.png' alt='Magreza' >´
         } else if (imc >= 18.5 && imc <= 24.9) {
-            imagemresultado.innerHTML = "<img src = 'images/m-normal.png' alt='Normal'>"
+            imagemresultado.innerHTML = ´<img src = 'images/m-normal.png' alt='Normal'>´
 
         } else if (imc >= 25 && imc <= 29.9) {
-            imagemresultado.innerHTML = "<img src = 'images/m-sobrepeso.png' alt='Sobrepeso'>"
+            imagemresultado.innerHTML = ´<img src = 'images/m-sobrepeso.png' alt='Sobrepeso'>´
 
         } else if (imc >= 30 && imc <= 39.9) {
-            imagemresultado.innerHTML = "<img src = 'images/m-obesidade.png' alt='Obesidade'>"
+            imagemresultado.innerHTML = ´<img src = 'images/m-obesidade.png' alt='Obesidade'>´
 
         } else if (imc > 40) {
-            imagemresultado.innerHTML = "<img src = 'images/m-obesidade-grave.png' alt='Obesidade'>"
+            imagemresultado.innerHTML = ´<img src = 'images/m-obesidade-grave.png' alt='Obesidade'>´
         }
     }
 
     if (sexo.value == 'feminino') {
         if (imc < 18.5) {
-            imagemresultado.innerHTML = "<img src = 'images/f-magreza.png' alt='Magreza'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-magreza.png' alt='Magreza'>´
 
         } else if (imc >= 18.5 && imc <= 24.9) {
-            imagemresultado.innerHTML = "<img src = 'images/f-normal.png' alt='Normal'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-normal.png' alt='Normal'>´
 
         } else if (imc >= 25 && imc <= 29.9) {
-            imagemresultado.innerHTML = "<img src = 'images/f-sobrepeso.png' alt='Sobrepeso'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-sobrepeso.png' alt='Sobrepeso'>´
 
         } else if (imc >= 30 && imc <= 34.9) {
-            imagemresultado.innerHTML = "<img src = 'images/f-obesidadeI.png' alt='ObesidadeI'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-obesidadeI.png' alt='ObesidadeI'>´
 
         } else if (imc >= 35 && imc <= 39.9) {
-            imagemresultado.innerHTML = "<img src = 'images/f-obesidadeII.png' alt='ObesidadeII'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-obesidadeII.png' alt='ObesidadeII'>´
 
         } else if (imc > 40) {
-            imagemresultado.innerHTML = "<img src = 'images/f-obesidadeIII.png' alt='ObesidadeIII'>"
+            imagemresultado.innerHTML = ´<img src = 'images/f-obesidadeIII.png' alt='ObesidadeIII'>´
         }
     }
 
